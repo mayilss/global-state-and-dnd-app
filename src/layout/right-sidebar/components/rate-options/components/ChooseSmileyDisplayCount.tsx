874b4d13521cbe4ec3176783b9b13d59bda07783
mayilss <mayilsafarzada@gmail.com>
2024-04-17 01:54:30 +0400

    RadioOption component added

--- src/layout/right-sidebar/components/rate-options/components/ChooseSmileyDisplayCount.tsx
+++ src/layout/right-sidebar/components/rate-options/components/ChooseSmileyDisplayCount.tsx
@@ -1,7 +1,7 @@
 import { colors } from "@constants";
 import { useGlobalState } from "@libs/global-state";
 import globalStore from "@store";
-import { Radio, RadioChangeEvent } from "antd";
+import { Radio, RadioChangeEvent, RadioProps } from "antd";
 import { Angry, Frown, Laugh, Meh, Smile } from "lucide-react";
 
 export default function ChooseSmileyDisplayCount() {
@@ -16,28 +16,22 @@ export default function ChooseSmileyDisplayCount() {
 
   return (
     <Radio.Group onChange={onDisplayCountChange} value={smileDisplayCount}>
-      <Radio style={{ width: "100%" }} value={5}>
-        <Laugh color={colors.grey} />
-        <Smile color={colors.grey} />
-        <Meh color={colors.grey} />
-        <Frown color={colors.grey} />
-        <Angry color={colors.grey} />
-      </Radio>
-      <Radio style={{ width: "100%" }} value={4}>
-        <Laugh color={colors.grey} />
-        <Smile color={colors.grey} />
-        <Frown color={colors.grey} />
-        <Angry color={colors.grey} />
-      </Radio>
-      <Radio style={{ width: "100%" }} value={3}>
-        <Laugh color={colors.grey} />
-        <Meh color={colors.grey} />
-        <Angry color={colors.grey} />
-      </Radio>
-      <Radio style={{ width: "100%" }} value={2}>
-        <Laugh color={colors.grey} />
-        <Angry color={colors.grey} />
-      </Radio>
+      <RadioOption value={5} />
+      <RadioOption value={4} />
+      <RadioOption value={3} />
+      <RadioOption value={2} />
     </Radio.Group>
+  );
+}
+
+function RadioOption({ value, ...props }: RadioProps) {
+  return (
+    <Radio style={{ width: "100%" }} value={value} {...props}>
+      <Laugh color={colors.grey} />
+      {value !== 3 && value !== 2 && <Smile color={colors.grey} />}
+      {value !== 4 && value !== 2 && <Meh color={colors.grey} />}
+      {value !== 3 && value !== 2 && <Frown color={colors.grey} />}
+      <Angry color={colors.grey} />
+    </Radio>
   );
 }
